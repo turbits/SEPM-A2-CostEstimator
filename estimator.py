c841c1474b24effd8f71c90c003960642ec100cb
Trevor <turbits@protonmail.com>
2023-11-21 11:47:08 -0700

    Update estimator.py

--- estimator.py
+++ estimator.py
@@ -1,9 +1,14 @@
+# determine if reading from csv or compiling and writing csv from user input
+
+# if collecting:
 # collect user inputted specifications for model
 # hardware
 # software
 # testing?
-
 # output provided spec to csv file
+
+# if reading:
+# read csv file and store in variables
 
 # generate 3point for hardware
 # generate 3point for software
