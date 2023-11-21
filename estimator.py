42dea3f98cec8a0342c9808f081b1ffe90c1b598
Trevor <turbits@protonmail.com>
2023-11-21 11:45:46 -0700

    wip outline of program

--- estimator.py
+++ estimator.py
@@ -1,1 +1,12 @@
-# -
+# collect user inputted specifications for model
+# hardware
+# software
+# testing?
+
+# output provided spec to csv file
+
+# generate 3point for hardware
+# generate 3point for software
+# generate 3point for testing
+
+# generate 3point for total
